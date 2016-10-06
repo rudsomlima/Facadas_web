897304b7273f46d65ff52d513fce9aed9bddc8a4
Rudsom <rudsomlima@gmail.com>
2016-10-06 16:43:53 -0300

    Aula 5

---
+++ src/main/java/ClienteServlet.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/cliente")
+public class ClienteServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Chamou pelo método GET");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//System.out.println("Chamou pelo método POST");
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método POST");
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método DELETE");
+	}
+
+}
